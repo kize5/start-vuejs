a104791589c5bee722364b531fbc6133d221fd62
kize5 <bochu.nicolas@hotmail.fr>
2023-02-02 13:58:47 +0100

    first commit - Ajout component ville dans villes

--- vue-project/src/components/City.vue
+++ vue-project/src/components/City.vue
@@ -1,19 +1,23 @@
 <template>
-<h1>Météo actuel de ma ville</h1>
-  <p> {{ name }}</p>
+  <div class="container">
+  <h1> {{ name }} </h1>
   <p> {{ weather }}</p>
   <p> {{ temperature }}</p>
   <p> {{ updatedAt }}</p>
+  </div>
 </template>
 
 <script>
 export default {
+  props: {
+    name: String, // nom de la ville
+    weather: String, // descriptif météo
+    temperature: Number + String, // température en °C
+    updatedAt: ""// date de dernière mise à jour
+  },
   data() {
     return {
-      name: 'Rumilly', // nom de la ville
-      weather: 'Soleil de dingue', // descriptif météo
-      temperature: 20.55 + "°", // température en °C
-      updatedAt: new Date().toLocaleString()// date de dernière mise à jour
+
     }
   }
 }
@@ -21,4 +25,7 @@ export default {
 
 <style scoped>
 
+.container {
+ margin: 5px;
+}
 </style>
--- vue-project/src/views/CitiesList.vue
+++ vue-project/src/views/CitiesList.vue
@@ -1,10 +1,21 @@
+
 <template>
-<h1>Météo des villes</h1>
+
+<h1>Météo des villes :</h1>
+  <City />
+  <City name="Annecy" />
+  <City weather="Soleil" />
+  <City temperature= 20° />
+  <City :updatedAt= "new Date().toLocaleString()" />
+
 </template>
 
 <script>
+import City from "@/components/City.vue";
 export default {
-  name: "CitiesList"
+  components :{
+    City
+  }
 }
 </script>
 
