6bc2b8f8c4e649bdebe7c88ea29c70b0a00a1fa2
kize5 <bochu.nicolas@hotmail.fr>
2023-02-02 10:06:20 +0100

    first commit - Ajout composant CitiesList

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,87 +1,42 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-import discover from "@/views/Discover.vue";
 import Discover from "@/views/Discover.vue";
+import CitiesList from "@/views/CitiesList.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+<!--  <header>-->
+<!--    <div class="wrapper">-->
+<!--      <nav>-->
+<!--      </nav>-->
+<!--    </div>-->
+<!--  </header>-->
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <CitiesList />
+  <main>
+    <div class="container">
+      <button id="btn" @click="alertMe">Clic me please !</button>
     </div>
-  </header>
+  </main>
 
   <RouterView />
 </template>
 
+<script>
+export default {
+  data(){},
+  methods: {
+    alertMe(){
+      alert("Useless alert pop")
+    }
+  }
+}
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+#btn {
+  height: 130px;
+  width: 130px;
 }
 </style>
