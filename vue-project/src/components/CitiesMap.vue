473e2679416993ba5edd0cb891e0a02569fe086a
kize5 <bochu.nicolas@hotmail.fr>
2023-02-06 11:40:16 +0100

    Sujet fini

--- vue-project/src/components/CitiesMap.vue
+++ vue-project/src/components/CitiesMap.vue
@@ -7,7 +7,15 @@
           name="OpenStreetMap"
       ></l-tile-layer>
       <div v-for="city in cities">
-      <l-marker :lat-lng="city.longlat"></l-marker>
+        <div v-if="city.meteo === 'soleil'">
+          <l-marker :lat-lng="city.longlat" :icon="iconSun"></l-marker>
+        </div>
+        <div v-if="city.meteo === 'couvert'">
+          <l-marker :lat-lng="city.longlat" :icon="iconCloud"></l-marker>
+        </div>
+        <div v-if="city.meteo === 'pluie'">
+          <l-marker :lat-lng="city.longlat" :icon="iconRain"></l-marker>
+        </div>
       </div>
     </l-map>
   </div>
@@ -15,7 +23,7 @@
 
 <script>
 import "leaflet/dist/leaflet.css";
-import {LMap, LTileLayer, LMarker} from "@vue-leaflet/vue-leaflet";
+import {LMap, LTileLayer, LMarker, LIcon} from "@vue-leaflet/vue-leaflet";
 
 const urlMeteo = 'https://api.openweathermap.org/data/2.5/find?lat=45.758&lon=4.765&cnt=20&cluster=yes&lang=fr&units=metric&APPID=32ca9eba476d69dd60c99ca49988792b'
 export default {
@@ -27,10 +35,28 @@ export default {
   data() {
     return {
       zoom: 11,
-      markerLatLng: [45.907742, 6.102371],
       cities: [],
+      weather: [],
       loading: false,
       error: null,
+      iconSun: L.icon({
+        iconUrl: '/node_modules/leaflet/dist/images/soleil.png',
+        iconSize: [32, 37],
+        iconAnchor: [16, 37]
+      }),
+      iconCloud: L.icon({
+        iconUrl: '/node_modules/leaflet/dist/images/nuage.png',
+        iconSize: [32, 37],
+        iconAnchor: [16, 37]
+      }),
+      iconRain: L.icon({
+        iconUrl: '/node_modules/leaflet/dist/images/pluie.png',
+        iconSize: [32, 37],
+        iconAnchor: [16, 37]
+      }),
+      staticAnchor: [16, 37],
+      customText: 'Foobar',
+      iconSize: 64
     };
   },
   created() {
@@ -46,6 +72,7 @@ export default {
           this.cities.push(
               {
                 longlat: city.coord,
+                meteo: city.weather[0].description
               }
           )
         }
